e023ed29ec6c806d566704adb4334ed0faceb2b9
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-10 17:25:11 +0900

    Initial Commit

---
+++ Acty/ActyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActyApp.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ActyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
