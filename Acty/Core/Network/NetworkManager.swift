17155bb9e013ed3663b2d1c37fad28f54c521ab4
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-12 23:52:27 +0900

    feat: NetworkManager 구현

--- Acty/Core/Network/NetworkManager.swift
+++ Acty/Core/Network/NetworkManager.swift
@@ -12,5 +12,23 @@ final class NetworkManager {
     static let shared = NetworkManager()
     private init() {}
     
-    func fetchResults() {}
+    func fetchResults<T: Decodable>(api: EndPoint) async throws -> T {
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(api.endPoint,
+                       method: api.method,
+                       parameters: api.parameters,
+                       encoding: api.encoding,
+                       headers: api.headers)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .success(let result):
+                    print(result)
+                    continuation.resume(returning: result)
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
 }
