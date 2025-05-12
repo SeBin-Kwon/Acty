840f9770522aba4850cd5c6ede451886d8e8f83e
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-12 22:48:43 +0900

    feat: NetworkManager 파일 추가

---
+++ Acty/Core/Network/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/12/25.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetchResults() {}
+}
