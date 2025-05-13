63e1eb9ab5f95f76802560a23c007c9f152475c6
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-14 00:53:46 +0900

    feat: 애플 로그인 구현

--- Acty/Presentation/Auth/Views/LoginView.swift
+++ Acty/Presentation/Auth/Views/LoginView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LoginView: View {
     var body: some View {
@@ -15,12 +16,45 @@ struct LoginView: View {
                     .imageScale(.large)
                     .foregroundStyle(.tint)
                 Text("Login")
+                AppleSigninButton()
                 NavigationLink("SignUp") {
                     SignUpView()
                 }
             }
             .padding()
         }
+    }
+}
+
+struct AppleSigninButton : View{
+    var body: some View{
+        SignInWithAppleButton(
+            onRequest: { request in
+                request.requestedScopes = [.fullName, .email]
+            },
+            onCompletion: { result in
+                switch result {
+                case .success(let authResults):
+                    print("Apple Login Successful")
+                    switch authResults.credential{
+                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                            let UserIdentifier = appleIDCredential.user
+                            let fullName = appleIDCredential.fullName
+                            let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                            let email = appleIDCredential.email
+                            let IdentityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                            let AuthorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                    default:
+                        break
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    print("error")
+                }
+            }
+        )
+        .frame(width : UIScreen.main.bounds.width * 0.9, height:50)
+        .cornerRadius(5)
     }
 }
 
