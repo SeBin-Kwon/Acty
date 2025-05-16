14abac86da3af74193f7c908da5199062a1dd3e8
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-16 20:03:33 +0900

    feat: SignInRequestDTO 추가

---
+++ Acty/Core/Models/SignInRequestDTO.swift
@@ -0,0 +1,25 @@
+//
+//  SignInRequestDTO.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/16/25.
+//
+
+import Foundation
+
+struct EmailSignInRequestDTO {
+    let email: String
+    let password: String
+    let deviceToken: String
+}
+
+struct AppleSignInRequestDTO {
+    let idToken: String
+    let deviceToken: String
+    let nick: String
+}
+
+struct KakaoSignInRequestDTO {
+    let oauthToken: String
+    let deviceToken: String
+}
