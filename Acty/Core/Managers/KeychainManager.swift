4b2b3b910d468aa4a7d39f9382eca07117d2a424
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-18 22:07:11 +0900

    feat: KeychainManager 파일 추가

---
+++ Acty/Core/Managers/KeychainManager.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainManager.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/18/25.
+//
+
+import Foundation
+
+final class KeychainManager {
+    static let shared = KeychainManager()
+    private init() {}
+    
+}
