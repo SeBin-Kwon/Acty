e93738d96d5742479d396cba8119b6320330a38d
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-20 00:57:22 +0900

    feat: EndPoint SignIn 업데이트

--- Acty/Core/Network/EndPoint.swift
+++ Acty/Core/Network/EndPoint.swift
@@ -10,6 +10,10 @@ import Alamofire
 
 enum EndPoint {
     case signUp(SignUpRequest)
+    case emailSignIn(EmailSignInRequestDTO)
+    case appleSignIn(AppleSignInRequestDTO)
+    case kakaoSignIn(KakaoSignInRequestDTO)
+    case refreshToken(String)
     
     var baseURL: String {
         BASE_URL
@@ -18,45 +22,57 @@ enum EndPoint {
     var endPoint: String {
         switch self {
         case .signUp: baseURL + "users/join"
+        case .emailSignIn: baseURL + "users/login"
+        case .appleSignIn: baseURL + "users/login/apple"
+        case .kakaoSignIn: baseURL + "users/login/kakao"
+        case .refreshToken: baseURL + "auth/refresh"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .signUp: .post
+        case .signUp, .emailSignIn, .appleSignIn, .kakaoSignIn, .refreshToken: .post
         }
     }
     
     var parameters: Parameters? {
         switch self {
         case .signUp(let request):
-            ["email": request.email,
-             "password": request.password,
-             "nick": request.nick,
-             "phoneNum": request.phoneNum,
-             "introduction": request.introduction,
-             "deviceToken": request.deviceToken]
+            return ["email": request.email,
+                   "password": request.password,
+                   "nick": request.nick,
+                   "phoneNum": request.phoneNum,
+                   "introduction": request.introduction,
+                   "deviceToken": request.deviceToken]
+        case .emailSignIn(let dto):
+            return ["email": dto.email,
+                   "password": dto.password,
+                   "deviceToken": dto.deviceToken ?? ""]
+        case .appleSignIn(let dto):
+            return ["idToken": dto.idToken,
+                   "deviceToken": dto.deviceToken ?? "",
+                   "nick": dto.nick ?? ""]
+        case .kakaoSignIn(let dto):
+            return ["oauthToken": dto.oauthToken,
+                   "deviceToken": dto.deviceToken ?? ""]
+        case .refreshToken(let token):
+            return ["refreshToken": token]
         }
     }
     
     var headers: HTTPHeaders {
         switch self {
-        case .signUp:
-            ["Content-Type": "application/json", "SeSACKey": API_KEY]
-
+        case .signUp, .emailSignIn, .appleSignIn, .kakaoSignIn, .refreshToken:
+            return ["Content-Type": "application/json", "SeSACKey": API_KEY]
         }
     }
     
     var encoding: ParameterEncoding {
         switch self {
-        case .signUp: JSONEncoding.default
+        case .signUp, .emailSignIn, .appleSignIn, .kakaoSignIn, .refreshToken:
+            return JSONEncoding.default
         }
     }
-    
-//    var error: Error.Type {
-//        switch self {
-//        }
-//    }
 }
 
 struct SignUpRequest {
