a135b33722f03ac188fc0754d1fb919a5d42f297
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-24 19:58:57 +0900

    feat: FlowRouter 추가

---
+++ Acty/Presentation/Navigation/FlowRouter.swift
@@ -0,0 +1,26 @@
+//
+//  Route.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/23/25.
+//
+
+import Foundation
+
+final class FlowRouter<T: Sendable>: Sendable {
+    @MainActor
+    private var continuation: AsyncStream<T>.Continuation?
+    
+    func `switch`(_ flow: T) async {
+        await continuation?.yield(flow)
+    }
+    
+    @MainActor
+    var stream: AsyncStream<T> {
+        return AsyncStream { [weak self] continuation in
+            Task { @Sendable in
+                self?.continuation = continuation
+            }
+        }
+    }
+}
