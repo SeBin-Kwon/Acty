ce9fee0184dd5bdd5eb97c471d561a9f719a1433
SeBin-Kwon <sbkwon98@gmail.com>
2025-05-26 23:58:42 +0900

    feat: RootView 추가

---
+++ Acty/Presentation/Common/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 5/26/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
