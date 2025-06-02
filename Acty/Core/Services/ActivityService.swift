fcecab4a90a4694e0ecee55126988cf7d0d35dd1
SeBin-Kwon <sbkwon98@gmail.com>
2025-06-02 23:27:37 +0900

    feat: ActivityService 구성

---
+++ Acty/Core/Services/ActivityService.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityService.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 6/2/25.
+//
+
+import Foundation
+
+protocol ActivityServiceProtocol {
+    func fetchActivities() -> [Activity]
+}
+
+final class ActivityService: ActivityServiceProtocol {
+    private let networkManager: NetworkManager
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchActivities() -> [Activity] {
+//        let result: ActivityDTO = try await networkManager.fetchResults(api: .activity)
+        return []
+    }
+}
