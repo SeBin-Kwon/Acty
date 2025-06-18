14cdbbb54adf5a892d91703025a7d345d3ff4ca5
SeBin-Kwon <sbkwon98@gmail.com>
2025-06-19 00:38:46 +0900

    feat: OrdersResponseDTO 업데이트

--- Acty/Core/Models/DTO/OrdersResponseDTO.swift
+++ Acty/Core/Models/DTO/OrdersResponseDTO.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct OrdersResponseDTO {
+    let id: String
+    let orderCode: String
+    let totalPrice: String
+    let createdAt: Int
+    let updatedAt: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "order_id"
+        case orderCode = "order_code"
+        case totalPrice = "total_price"
+        case createdAt, updatedAt
+    }
+}
