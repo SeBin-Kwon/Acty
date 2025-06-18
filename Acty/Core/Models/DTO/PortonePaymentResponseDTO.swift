b5ec3f361ca994a8e92225a56497c33a54add01e
SeBin-Kwon <sbkwon98@gmail.com>
2025-06-19 01:03:47 +0900

    feat: PortonePaymentResponseDTO 업데이트

--- Acty/Core/Models/DTO/PortonePaymentResponseDTO.swift
+++ Acty/Core/Models/DTO/PortonePaymentResponseDTO.swift
@@ -7,23 +7,10 @@
 
 import Foundation
 
-struct PortonePaymentResponseDTO: Codable {
+struct PortonePaymentResponseDTO {
     let success: Bool
     let merchantUid: String
     let impUid: String?
-    let payMethod: String?
-    let amount: Int?
-    let status: String?
-    let name: String?
-    let paidAt: Int?
-    let receiptUrl: String?
     let errorCode: String?
     let errorMsg: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case success, merchantUid = "merchant_uid", impUid = "imp_uid"
-        case payMethod = "pay_method", amount, status, name
-        case paidAt = "paid_at", receiptUrl = "receipt_url"
-        case errorCode = "error_code", errorMsg = "error_msg"
-    }
 }
