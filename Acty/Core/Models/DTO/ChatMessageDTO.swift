6f246674f9f913435ea0796256698effa5e9b211
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-06 23:58:42 +0900

    feat: ChatMessageDTO 추가

--- Acty/Core/Models/DTO/ChatMessageDTO.swift
+++ Acty/Core/Models/DTO/ChatMessageDTO.swift
@@ -1,8 +1,17 @@
 //
-//  ChatMassageDTO.swift
+//  ChatMessageDTO.swift
 //  Acty
 //
 //  Created by Sebin Kwon on 7/6/25.
 //
 
 import Foundation
+
+struct ChatMessageDTO: Codable {
+    let content: String
+    let files: [ChatFiles]
+}
+
+struct ChatFiles: Codable {
+    let url: String
+}
