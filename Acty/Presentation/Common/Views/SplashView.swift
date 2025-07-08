44b239c29bf7e26b4f4e63e464641587033a20d7
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-08 20:31:22 +0900

    feat: SplashView UI 업데이트

--- Acty/Presentation/Common/Views/SplashView.swift
+++ Acty/Presentation/Common/Views/SplashView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         VStack {
-            Image(systemName: "star.fill")
-                .font(.system(size: 60))
-            Text("Acty")
-                .font(.largeTitle)
-                .fontWeight(.bold)
+            Image(systemName: "burst.fill")
+                .font(.system(size: 50))
+                .foregroundColor(.accent)
+            
+            Text("ACTY")
+                .font(.paperLogy(.custom(.Black, CGFloat(40))))
+                .foregroundColor(.accent)
         }
     }
 }
