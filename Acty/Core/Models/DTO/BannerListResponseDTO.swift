f9ed377fb2425767526874b0af3c80f7abb19db5
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-20 20:48:33 +0900

    feat: Banner Entity 추가

--- Acty/Core/Models/DTO/BannerListResponseDTO.swift
+++ Acty/Core/Models/DTO/BannerListResponseDTO.swift
@@ -21,3 +21,16 @@ struct PayloadDTO: Decodable {
     let type: String
     let value: String
 }
+
+extension BannerDTO {
+    func toEntity() -> Banner {
+        return Banner(
+            name: name,
+            imageUrl: imageUrl,
+            payload: Payload(
+                type: PayloadType(rawValue: payload.type) ?? .webview,
+                value: payload.value
+            )
+        )
+    }
+}
---
+++ Acty/Core/Models/Entity/Banner.swift
@@ -0,0 +1,28 @@
+//
+//  Banner.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 7/20/25.
+//
+
+import Foundation
+
+struct Banner: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let imageUrl: String
+    let payload: Payload
+    
+    var fullImageURL: String {
+        return BASE_URL + imageUrl
+    }
+}
+
+struct Payload: Hashable {
+    let type: PayloadType
+    let value: String
+}
+
+enum PayloadType: String {
+    case webview = "WEBVIEW"
+}
