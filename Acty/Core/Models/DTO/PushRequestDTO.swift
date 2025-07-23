88de3e134353a44aa737641ef378b5dc0319115c
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-23 16:09:15 +0900

    feat: PushRequestDTO 추가

---
+++ Acty/Core/Models/DTO/PushRequestDTO.swift
@@ -0,0 +1,19 @@
+//
+//  PushRequestDTO.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 7/23/25.
+//
+
+import Foundation
+
+struct PushRequestDTO: Encodable {
+    let userIds: [String]
+    let title: String
+    let subtitle: String
+    let body: String
+    enum CodingKeys: String, CodingKey {
+        case userIds = "user_ids"
+        case title, subtitle, body
+    }
+}
