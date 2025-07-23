a63fa6bdc5880daaecb55194fdf7bc1d185698d0
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-23 22:06:08 +0900

    feat: ChatFileResponseDTO 추가

---
+++ Acty/Core/Models/DTO/ChatFileResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ChatFileResponseDTO.swift
+//  Acty
+//
+//  Created by Sebin Kwon on 7/23/25.
+//
+
+import Foundation
+
+struct ChatFileResponseDTO: Decodable {
+    let files: [String]
+}
