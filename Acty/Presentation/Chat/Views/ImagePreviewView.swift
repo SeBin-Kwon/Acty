4384658c38923b762086a8be8a8f3e5cbdfd9f22
SeBin-Kwon <sbkwon98@gmail.com>
2025-07-24 21:20:35 +0900

    feat: ImagePreviewView 추가

--- Acty/Presentation/Chat/Views/ImagePreviewView.swift
+++ Acty/Presentation/Chat/Views/ImagePreviewView.swift
@@ -5,4 +5,43 @@
 //  Created by Sebin Kwon on 7/24/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ImagePreviewView: View {
+    let selectedImages: [Data]
+    let onRemove: (Int) -> Void
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(Array(selectedImages.enumerated()), id: \.offset) { index, imageData in
+                    ZStack(alignment: .topTrailing) {
+                        // 이미지 미리보기
+                        if let uiImage = UIImage(data: imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 80, height: 80)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        }
+                        
+                        // 삭제 버튼
+                        Button {
+                            onRemove(index)
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.gray60)
+                                .background(Color.white, in: Circle())
+                        }
+                        .offset(x: 5, y: -5)
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 20)
+        }
+        .frame(height: 100)
+        .padding(.vertical, 8)
+        .background(Color.white)
+    }
+}
